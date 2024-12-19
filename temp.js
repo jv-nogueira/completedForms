00f268e64011a3570192e2a074b82b4c6e972217
Joao Vitor Nogueira da Silva <joao.nogueira@digisystem.com.br>
2024-12-19 16:53:53 -0300

    Atualiza com os scripts que faltam para finalizar

--- temp.js
+++ temp.js
@@ -1,8 +1,4 @@
 
-
-// Na coluna F da planilha tem o título "Categoria". Se algum da lista incluir a mesma palavra, entao dar Enter
-document.querySelectorAll("[for='request-type-select']")[0].parentElement.parentElement.children[1].children[i].children[0].children[0].children[1].children[0].textContent
-
 // Na coluna D da planilha que tem o título "Assunto" setar o mesmo da planilha no script
 document.querySelector('#pf-undefined-tl-1').value = 'Ola'
 
